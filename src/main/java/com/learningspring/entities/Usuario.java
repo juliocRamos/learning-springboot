1959e9b1a78c29e3e02eaa714d19563a6b5053d6
Júlio Ramos <ps2.ramos@gmail.com>
2020-02-16 21:12:40 -0300

    Adição da JPA e base de testes H2 no projeto
    Motivo: I/M (M - Melhoria | C - Correção | I - Implementação)
    
    Descrição:
      - Adicionadas dependências para a JPA e H2.
      - A entidade Usuario foi mapeada com as annotations para que, ao
      executar o projeto a tabela referente seja criada.
    
    Observações:
    
    Referência:

--- src/main/java/com/learningspring/entities/Usuario.java
+++ src/main/java/com/learningspring/entities/Usuario.java
@@ -2,18 +2,31 @@ package com.learningspring.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(name="nome", nullable = false)
 	private String nome;
 	
+	@Column(name="email", nullable = true)
 	private String email;
 	
+	@Column(name="telefone", nullable = true)
 	private String telefone;
 	
+	@Column(name="senha", nullable = false)
 	private String senha;
 
 	public Usuario() {
